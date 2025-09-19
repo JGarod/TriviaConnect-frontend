83224a611d7f96497221cca222e3bc15024c0ace
Juan Garod <garode01@gmail.com>
2025-09-18 21:59:00 -0500

    ajustes vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://triviaconnect-backendz.onrender.com/api/auth',
+    apiUrl: 'https://triviaconnect-backendz.onrender.com/api',
     cloudinary_path: 'https://res.cloudinary.com/dhwg9lvhl/image/upload/f_jpg/perfiles/'
 };
